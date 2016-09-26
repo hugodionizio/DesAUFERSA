900c7fe43ecb35a8a50e45f6a661b634ad85d083
Hugo Dionizio Santos <dnhutos@hotmail.com>
2016-09-26 15:40:22 -0300

    Inicializando trabalho da Unidade II

---
+++ src/trabalhoUnidII/UnidadeII.java
@@ -0,0 +1,87 @@
+/*(NULL)*/
+class Colecao {
+	protected int rosca[];
+	protected int parafuso[];
+
+	public Colecao() {
+		
+	}
+
+	public void ordenarRoscasParafusos () {
+		
+	}
+
+}
+
+/*(NULL)*/
+class Vetor {
+	protected int v[];
+
+	public Vetor() {
+		
+	}
+
+	public int insertionBSearch (int a) {
+		
+	}
+
+	public void insertionSort (int a) {
+		
+	}
+
+	public int binarySearch (int a, int b) {
+		
+	}
+
+}
+
+/*(NULL)*/
+class Hash {
+	protected int listaHeap[];
+
+	public Hash() {
+		
+	}
+
+	public int encontrarK (int a) {
+		
+	}
+
+}
+
+/*(NULL)*/
+class Heap {
+	protected int altura;
+	protected int valor;
+	protected Heap pai;
+	protected Heap filhoEsq;
+	protected Heap filhoDir;
+
+	public Heap() {
+		
+	}
+
+	public void inserir (int a) {
+		
+	}
+
+}
+
+/*(NULL)*/
+class Intervalo {
+	protected int v[];
+
+	public Intervalo() {
+		
+	}
+
+	public void ordenar () {
+		
+	}
+
+	public int buscaBMenor (int a) {
+		
+	}
+
+}
+
