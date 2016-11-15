294a5fc3170c855ee101c4d1a498ec546b6a59ce
Hugo Dionizio Santos <dnhutos@hotmail.com>
2016-11-15 12:14:14 -0300

    Implementação do Problema das 15 Alunas

--- src/trabalhoUnidIII/Problema15Alunas.java
+++ src/trabalhoUnidIII/Problema15Alunas.java
@@ -10,17 +10,55 @@ package trabalhoUnidIII;
  * @author hugo
  */
 class Problema15Alunas {
+
     protected String[] alunas;
     protected String escala;
 
     public Problema15Alunas() {
     }
 
-    /** Busca em um vetor ordenado por inserção */
+    /**
+     * Busca em um vetor ordenado por inserção
+     */
     public static void inserirAluna(String a) {
     }
 
     public void organizarEscala() {
     }
-    
+
+    public static void main(String[] args) {
+        String alunas[] = {"Ana", "Beatriz", "Carla", "Diana", "Eva", "Francisca",
+            "Geane", "Helga", "Iana", "Jeane", "Katia", "Luiza", "Maria", "Najla",
+            "Olívia"};
+        String dias[] = {"Seg", "Ter", "Qua", "Qui", "Sex", "Sab", "Dom"};
+        int comb = 0;
+
+        System.out.println("Problema das 15 Alunas");
+        for (int i = 0; i < alunas.length; i += 3) {
+            System.out.println(alunas[i] + ", " + alunas[i + 1] + ", " + alunas[i + 2]);
+        }
+
+        for (int i = 0; i < alunas.length; i += 3) {
+            System.out.println(alunas[i].charAt(0) + "" + alunas[i + 1].charAt(0)
+                    + "" + alunas[i + 2].charAt(0));
+        }
+        
+        for (int i = 0; i < alunas.length-2; i++) {
+            for (int j = 1; j < alunas.length-1; j++) {
+                for (int k = 2; k < 10; k++) {
+                System.out.println(alunas[i].charAt(0) + "" + alunas[j].charAt(0)
+                    + "" + alunas[k].charAt(0));
+                comb++;
+                }                
+            }
+        }
+        
+        System.out.println("Número de combinações: "+comb);
+        
+        for (int i = 0; i < dias.length; i++) {
+            System.out.print(dias[i]+"|");
+        }
+        System.out.println("");
+    }
+
 }
