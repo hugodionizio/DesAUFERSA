e8e911df786791562b4282cdcc2cac4f492033b1
Hugo Dionizio Santos <dnhutos@hotmail.com>
2016-11-20 14:54:35 -0300

    Problema da Fórmula de Horn

--- src/trabalhoUnidIII/FormulasHorn.java
+++ src/trabalhoUnidIII/FormulasHorn.java
@@ -18,4 +18,81 @@ class FormulasHorn {
     public void inserir(int a) {
     }
     
+    public int numVariaveis(String formulaHorn) {
+        int numVariaveis = 0;
+        
+        return numVariaveis;
+    }
+    
+    public int numImplicacoes(String formulaHorn) {
+        int numImplicacoes = 0;
+        
+        return numImplicacoes;
+    }
+    
+    public int numClausulaPuramenteNegativas(String formulaHorn) {
+        int numClausulaPuramenteNegativas = 0;
+        
+        return numClausulaPuramenteNegativas;
+    }
+    
+    public boolean verificarFormula (String formulaHorn) {
+        boolean sucesso = false;
+        
+        int numVar = numVariaveis(formulaHorn);
+        int numImplic = numImplicacoes(formulaHorn);
+        int numClausulasPN = numClausulaPuramenteNegativas(formulaHorn);
+        boolean variaveis[] = new boolean[numVar];
+        boolean implicacoes[] = new boolean[numImplic];
+        boolean clausulaPN[] = new boolean[numClausulasPN];
+        
+        // Todas variáveis são consideradas falsas
+        for (int i = 0; i < numVar; i++) {
+            variaveis[i] = false;
+        }
+        
+        for (int i = 0; i < numImplic; i++) {
+            implicacoes[i] = false;
+        }
+        
+        for (int i = 0; i < numClausulasPN; i++) {
+            clausulaPN[i] = false;
+        }
+        
+        int j = 0;
+        // Enquanto existir implicações não satisfeitas
+        while(!implicacoes[j]) {
+            // Fazer a variável à direita da implicacação verdadeira
+            implicacoes[j+1] = true;
+            j++;
+        }
+	
+        // Se todas as cláusulas puramente negativas satisfeitas
+        for (int i = 0; i < numClausulasPN; i++) {
+            if (clausulaPN[i])
+        	// Retornar atribuição
+                sucesso = true;
+            else { // senão
+        	//retornar "Esta fórmula Horn não satisfeita"
+                sucesso = false;
+                i = numClausulasPN;
+            }
+        }        
+        
+        return sucesso;
+    }
+    
+    public static void main(String[] args) {
+        String formula = "(x^y^z)=>, (x^z)=>w, x=>y, =>x, (x^y)=>w, (~wV~xV~y), (~z)";
+        
+        FormulasHorn f = new FormulasHorn();
+        
+        if(f.verificarFormula(formula)) {
+            System.out.println(formula);   
+        }
+        else {
+            System.out.println("Esta fórmula Horn não satisfeita");
+        }
+    }
+    
 }
